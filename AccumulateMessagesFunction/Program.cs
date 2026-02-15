94cac3cb4d56a22f2da93536e3a6a833b569b12c
abeldoyl <abeldoyle71@gmail.com>
2026-02-15 10:54:44 -0700

    Add project files.

---
+++ AccumulateMessagesFunction/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace AccumulateMessagesFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            string message = "";
+            string addString = message + userInput;
+
+            Console.WriteLine("Enter a new message");
+            Console.WriteLine($"To quit press \"Q\"");
+
+            do
+            {
+                userInput = Console.ReadLine();
+
+                if ((userInput.Equals("Q", StringComparison.OrdinalIgnoreCase)))
+                {
+                    if (string.IsNullOrEmpty(message))
+                    {
+                        message = userInput;
+                    }
+
+                    else
+                    {
+                        message = message + "\n" + userInput;
+                    }
+                    addString = message;
+                }
+                Console.Clear();
+                Console.WriteLine(addString);
+            }
+        }
+    }
+}
