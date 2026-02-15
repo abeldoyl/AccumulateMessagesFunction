ad59488642b2f725391ee2e8974831472e813885
abeldoyl <abeldoyle71@gmail.com>
2026-02-15 12:17:56 -0700

    add functionality and comments

--- AccumulateMessagesFunction/Program.cs
+++ AccumulateMessagesFunction/Program.cs
@@ -15,13 +15,14 @@
             {
                 userInput = Console.ReadLine();
 
-                if ((userInput.Equals("Q", StringComparison.OrdinalIgnoreCase)))
+                if ((userInput != "Q" && userInput != "q"))
                 {
                     if (string.IsNullOrEmpty(message))
                     {
                         message = userInput;
                     }
 
+                    //adds new line if input is left blank
                     else
                     {
                         message = message + "\n" + userInput;
@@ -30,7 +31,15 @@
                 }
                 Console.Clear();
                 Console.WriteLine(addString);
-            }
+            } while (userInput != "Q" && userInput != "q");
+            
+            //Displays message before quitting
+            Console.Clear();
+            Console.WriteLine($"Your message is: \n{addString}");
+            Console.WriteLine();
+
+            //pause
+            Console.Read();
         }
     }
 }
